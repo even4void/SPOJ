ba62977274afc3dc6210407dc867c8457809acf5
chl <chl@aliquote.org>
2019-10-04 15:38:45 +0200

    Fix output + Add one-liner test

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 # Usage:
-# $ echo '1 2 88 42 99' | python3 test.py
+# $ cat test.in | python3 test.py | diff - test.out
 
 
 import sys
 
 
 def main():
-    data = map(int, sys.stdin.readline().split())
-    for elt in data:
-        if elt == 42:
+    for line in sys.stdin:
+        if int(line) == 42:
             break
-        print(elt)
+        print(line.rstrip())
 
 
 if __name__ == '__main__':
